b820a379ed3a4f218f73e3cf7c8602ad1386f539
Mario Bodemann <mario.bodemann@telekom.de>
2023-02-03 12:13:19 +0100

    add documentation + type hints

--- camara.py
+++ camara.py
@@ -1,24 +1,71 @@
+"""
+CAMARA API Client
+
+This small SDK abstracts from the defined [CAMARA](https://github.com/camaraproject)-apis.
+
+Currently only the authentication and [Quality on Demand](https://github.com/camaraproject/QualityOnDemand) apis are
+supported,
+"""
+
 import datetime
 import json
 
 import requests
+from enum import Enum
 
+# base url for authentication
 OPENID_CONNECT_URL = "https://playground.spacegate.telekom.de/auth/realms/default/protocol/openid-connect/token"
 
 
 class Camara:
-    def __init__(self,
-                 client_id,
-                 client_secret,
-                 authentication_url=OPENID_CONNECT_URL):
-        self.authentication_url = authentication_url
-        self.token = None
-        self.authentication_responses = []
-        self.client_id = client_id
-        self.client_secret = client_secret
-        self.qod = QualityOnDemand(self)
+    """
+    Foundational class for all interactions with the CAMARA apis.
+
+    It takes care of the authentication and renewal of tokens and provides an api for the specific sub Camara
+    specification apis.
+
+    Currently only "QoD" is supported by using the `qod` field.
+
+    Usage:
+
+    ```
+    Camara(id, secret).qod.create_session("QOD_E")
+    ```
+    This will first create a new authentication token using client id and client secret, and then use this to create
+    a new qod session with priority of _QOD_E_.
+    """
+
+    def __init__(
+            self,
+            client_id: str,
+            client_secret: str,
+            authentication_url: str = OPENID_CONNECT_URL
+    ):
+        """
+        Create a new CAMARA client.
+
+        :param client_id: the client id specified by your CAMARA representative.
+        :param client_secret: the secret you got together with the client id.
+        :param authentication_url: Address of the authentication server. Defaults to DT authentication.
+        """
+
+        self.authentication_url: str = authentication_url
+        self.token: dict | None = None
+        self.authentication_responses: list = []
+        self.client_id: str = client_id
+        self.client_secret: str = client_secret
+        self.qod: QualityOnDemand = QualityOnDemand(self)
 
     def create_access_token(self):
+        """
+        Uses the client secret and client id to request an authentication token.
+
+        Upon successful request, the token is stored internally. Once created,
+         it's :func:`time left <camara.Camara.token_seconds_remaining>` and
+         :func:`expiration state <camara.Camara.is_token_expired>` can be retrieved.
+
+        :return: the request, response tuple of the operation.
+        """
         payload = f'grant_type=client_credentials&client_id={self.client_id}&client_secret={self.client_secret}'
         headers = {'Content-Type': 'application/x-www-form-urlencoded'}
 
@@ -36,17 +83,43 @@ class Camara:
         return response.request, response.json()
 
     def refresh_token(self):
+        """
+        Update token, if expired.
+
+        If the token is expired or does not exist yet, this method will create a new access token.
+
+        :return: request, response for this request, or none, none if no new token is required
+        """
         if not self.token or self.is_token_expired():
-            _, response = self.create_access_token()
+            request, response = self.create_access_token()
             self.token = response
+            return request, response
+        else:
+            return None, None
 
     def get_access_token(self):
+        """
+        Returns a potentially set token.
+
+        If the tokoen is already created or refreshed, it will get returned. Otherwise a None will get returned.
+
+        :return: a created access_token or None
+        """
         if "access_token" in self.token:
             return self.token["access_token"]
         else:
-            return "no token"
+            return None
 
-    def get_auth_headers(self, more=None):
+    def get_auth_headers(self, more: dict | None = None):
+        """
+        Create authentication headers and merge them with the 'more' headrs.
+
+        Use this method to create the authentication token header. If you got more headers to be added to this request
+        please specify them as a dictionary using the 'more' - field.
+
+        :param more: A dict containing more headers
+        :return: a dict including auth headers and more headers to be added
+        """
         merged = {
             'Authorization': f'Bearer {self.get_access_token()}',
         }
@@ -57,6 +130,11 @@ class Camara:
         return merged
 
     def is_token_expired(self):
+        """
+        Does an access token exist and is it not expired?
+
+        :return: whether the access token is invalid or expired
+        """
         if self.token and 'expires_at' in self.token:
             seconds_left = self.token_seconds_remaining()
             return seconds_left < 0
@@ -64,6 +142,11 @@ class Camara:
             return True
 
     def token_seconds_remaining(self):
+        """
+        How many seconds is the access token still valid?
+
+        :return: seconds of validity, or 0 if no access token is created.
+        """
         if self.token and 'expires_at' in self.token:
             return (self.token['expires_at'] - datetime.datetime.now()).total_seconds()
         else:
@@ -71,19 +154,44 @@ class Camara:
 
 
 class QualityOnDemand:
+    """
+    Specific CAMARA API: Quality on Demand
+
+    This set of rest operations control the quality on demand aspects of a network channel. It prioritizes network
+    traffic based on a specific qod priority class.
+    """
+
+    class Profile(Enum):
+        """
+        Enumeration holding all valid values for QoD profiles
+        """
+        E = "QOD_E"
+        S = "QOD_S"
+        M = "QOD_M"
+        L = "QOD_L"
+
     def __init__(self, token_provider):
         self.token_provider = token_provider
-        self.last_session = None
-        self.responses = []
-        self.base_url = "https://playground.spacegate.telekom.de/hnce/qod/v0/sessions"
+        self.last_session: dict | None = None
+        self.responses: list = []
+        self.base_url: str = "https://playground.spacegate.telekom.de/hnce/qod/v0/sessions"
 
     def create_session(
             self,
-            qos,
-            from_ip,
-            to_ip="0.0.0.0/0",
-            duration=10,
+            qos: Profile,
+            from_ip: str,
+            to_ip: str = "0.0.0.0/0",
+            duration: int = 10,
     ):
+        """
+        Create a new qod session, prioritizing traffic.
+
+        :param qos: QOD_E, QOD_S, QOD_M, QOD_L?
+        :param from_ip:
+        :param to_ip:
+        :param duration:
+        :return: request, response tuple for the actual rest call
+        """
         self.token_provider.refresh_token()
 
         payload = json.dumps({
@@ -94,7 +202,7 @@ class QualityOnDemand:
             "asId": {
                 "ipv4addr": to_ip
             },
-            "qos": qos
+            "qos": qos.value
         })
 
         headers = self.token_provider.get_auth_headers({'Content-Type': 'application/json'})
@@ -111,14 +219,26 @@ class QualityOnDemand:
 
         return response.request, response.json()
 
-    def delete_session(self, session_id):
+    def delete_session(self, session_id: str):
+        """
+        Deletes the session as identified by its session id.
+
+        :param session_id: the id of the session to be deleted
+        :return: request, response of the operation call
+        """
         self.token_provider.refresh_token()
 
         url = f"{self.base_url}/{session_id}"
         response = requests.request("DELETE", url, headers=self.token_provider.get_auth_headers())
         return response.request, response
 
-    def get_session(self, session_id):
+    def get_session(self, session_id: str):
+        """
+        Returns the session identified by the given id.
+
+        :param session_id: Which session to return?
+        :return: the request and response(session) of this call.
+        """
         self.token_provider.refresh_token()
 
         url = f"{self.base_url}/{session_id}"
@@ -129,6 +249,11 @@ class QualityOnDemand:
         return request, response
 
     def is_session_expired(self):
+        """
+        Is the last created session still active?
+
+        :return: False if the session exists and is still active, otherwise True.
+        """
         if self.last_session and 'expires_at' in self.last_session:
             seconds_left = self.session_seconds_remaining()
             return seconds_left < 0
@@ -136,6 +261,10 @@ class QualityOnDemand:
             return True
 
     def session_seconds_remaining(self):
+        """
+        How long is the last created session still active? (in seconds)
+        :return: seconds remaining on the active session, or 0 if no session exists.
+        """
         if self.last_session and 'expires_at' in self.last_session:
             return (self.last_session['expires_at'] - datetime.datetime.now()).total_seconds()
         else:
--- cli.py
+++ cli.py
@@ -1,32 +1,67 @@
 #!/usr/bin/env python3
+"""
+This is the entry point for the CAMARA APIs.
+
+It provides a cli when called as a script, and calls apis specified in [CAMARA](https://github.com/camraproject).
+"""
 import os
 from camara import *
 
-COLOR = True
+# Set to 'None' to disable color. Otherwise, use an ANSI escape color code
+# number from here: https://en.wikipedia.org/wiki/ANSI_escape_code
+INFO_COLOR = 32
+ERROR_COLOR = 41
+COLOR = INFO_COLOR
 
 
-def colorize(text):
-    if COLOR:
-        return f"\033[32m{text}\033[m"
+def colorize(text, color=COLOR):
+    """
+    Take the text and colorize it with one color.
+
+    This function can be used to colorize a piece of text for console output.
+
+    :param text: which text to colorize
+    :param color: which color to be taken.
+    :return: a colorized version of the input text.
+    """
+    if color:
+        return f"\033[{color}m{text}\033[m"
     else:
         return text
 
 
+# Prompt for the user: Whenever this CLI asks the user something, this prompt appears
 PROMPT = f"{colorize('CAMARA')}> "
 
+# Client details for authentication. Please ask your CAMARA representative for these values.
 CAMARA_CLIENT_SECRET_ENV = 'CAMARA_CLIENT_SECRET'
 CAMARA_CLIENT_ID_ENV = 'CAMARA_CLIENT_ID'
 
 
 def menu(client_id, client_secret):
+    """
+    Creates a nice menu for the Command Line Interface.
+
+    :param client_id: which id to be used to authenticate
+    :param client_secret:  the secret to be used for authentication
+    :return: nothing - on return the script ends
+    """
+
     class Config:
+        """
+        Configuration Abstraction
+
+        Used internally to store the configuration values for the menu. Think of ip addresses and other values needed
+        to execute apis.
+        """
+
         def __init__(self):
             self.duration = 10
             self.from_ip = "127.0.0.1"
             self.to_ip = "0.0.0.0/0"
 
     def user_help():
-        """Print help."""
+        """Print help for all verbs."""
 
         print(f"Available verbs:")
         for key in verbs.keys():
@@ -60,17 +95,17 @@ def menu(client_id, client_secret):
         return True
 
     def user_set_to():
-        """Set to ip address."""
+        """Set to ip address. The ip of the device the connection should start from. Alternative name: ue address."""
         config.to_ip = input(f"ip (currently {config.to_ip})? ")
         return True
 
     def user_set_from():
-        """Set from ip address."""
+        """Set from ip address. The ip the device connects to. Alternative name: as address."""
         config.from_ip = input(f"ip (currently {config.from_ip})? ")
         return True
 
     def user_set_duration():
-        """Set duration in seconds."""
+        """Set duration of newly created sessions in seconds."""
         try:
             config.duration = int(input(f"duration (currently {config.duration})? "))
         except ValueError:
@@ -78,13 +113,13 @@ def menu(client_id, client_secret):
         return True
 
     def user_create_token():
-        """Create a new token."""
+        """Create a new access token. Needs client id and client secret for success."""
         request, response = camara.create_access_token()
         print_request_response(request, response)
         return True
 
-    def create_session(qos):
-        """Create a qos session using one of the defined classes, the to and from ip address and a given duration."""
+    def create_session(qos: QualityOnDemand.Profile):
+        """Create a qos session using one of the defined qos classes. Needs 'from', 'to', and 'duration' values."""
         request, response = qod.create_session(
             qos=qos,
             from_ip=config.from_ip,
@@ -121,10 +156,11 @@ def menu(client_id, client_secret):
         return True
 
     def user_exit():
-        """Closes this program."""
+        """Close this cli."""
         return False
 
     def print_request_response(request, response):
+        """Helper function for printing the request and response, humanfriendly."""
         print(f"request: {request.body}")
         if 'status' in response:
             status = response['status']
@@ -133,10 +169,12 @@ def menu(client_id, client_secret):
         print(f"response: {status} {response}")
 
     def create_session_creation_function(qod_for_lambda):
+        """Create a function that calls the creation of a session. Used for user interaction."""
+
         def inline():
             return create_session(qod_for_lambda)
 
-        inline.__doc__ = f"Creates a session of type '{qod_for_lambda}'."
+        inline.__doc__ = f"Create a qos session of type '{qod_for_lambda}'."
 
         return inline
 
@@ -148,10 +186,10 @@ def menu(client_id, client_secret):
         'qod from': user_set_from,
         'qod duration': user_set_duration,
         'qod delete': user_delete_session,
-        'qod e': create_session_creation_function("QOS_E"),
-        'qod s': create_session_creation_function("QOS_S"),
-        'qod m': create_session_creation_function("QOS_M"),
-        'qod l': create_session_creation_function("QOS_L"),
+        'qod e': create_session_creation_function(QualityOnDemand.Profile.E),
+        'qod s': create_session_creation_function(QualityOnDemand.Profile.S),
+        'qod m': create_session_creation_function(QualityOnDemand.Profile.M),
+        'qod l': create_session_creation_function(QualityOnDemand.Profile.L),
         'time': user_time,
         'info': user_info,
     }
@@ -162,19 +200,26 @@ def menu(client_id, client_secret):
     config = Config()
 
     print()
-    print("Welcome to the CAMARA management command line interface.")
+    print(colorize("Welcome to the CAMARA management command line interface."))
     print()
 
+    # loop till either _exit_ is typed, an uncaught exception is thrown, or one of the _user*_ functions returns false
     go_on = True
     while go_on:
         selection = input(PROMPT)
         if selection in verbs:
-            go_on = verbs[selection]()
+            try:
+                go_on = verbs[selection]()
+            except Exception as exception:
+                # be graceful with exceptions: Print them and don't explode the cli.
+                print(f"{colorize('Exception caught', ERROR_COLOR)}: {exception}.")
         else:
-            print("Unknown verb. Try 'help'.")
+            print("Unknown verb. Try 'help' to list all the supported _verbs_.")
 
 
+# Are we in interactive / scripting mode?
 if __name__ == "__main__":
+    # CLI requested.
     if CAMARA_CLIENT_ID_ENV in os.environ:
         client_id = os.environ[CAMARA_CLIENT_ID_ENV]
     else:
@@ -187,4 +232,5 @@ if __name__ == "__main__":
 
     menu(client_id, client_secret)
 else:
-    print("Error: Please execute as '__main__'.")
+    # Invocation of cli module is discouraged
+    print("Error: Please execute as '__main__', or import 'camara'.")
