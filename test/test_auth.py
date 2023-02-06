f00dc6c19fcc5050742923f99e3c62182eac616b
Mario Bodemann <mario.bodemann@gmail.com>
2023-02-06 13:20:32 +0100

    add red tests

---
+++ test/test_auth.py
@@ -0,0 +1,12 @@
+import unittest
+import camara
+
+def test_no_reauth():
+    assert 'Not Implemented' == True
+
+def test_auth_if_expired():
+    assert 'Not Implemented' == True
+
+def test_auth_if_notset():
+    assert 'Not Implemented' == True
+
